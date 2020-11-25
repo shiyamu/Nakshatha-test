102d3b9b51768a76f291fd31ebdf83626f96fe01
shiyamu <shiyamu.dss@gmail.com>
2020-11-25 14:48:55 +0530

    Platform added

--- sketch.js
+++ sketch.js
@@ -16,7 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(180,300,300,180);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -57,6 +58,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    platform.display();
 
     bird.display();
 }
